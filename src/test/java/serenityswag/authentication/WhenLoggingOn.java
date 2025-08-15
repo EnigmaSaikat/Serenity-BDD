c736874e7e3dd1f080b0e4b1816716400c2fa75f
EnigmaSaikat1 <saikat.j.das@gmail.com>
2025-08-15 10:42:14 +0530

    Basic test structure

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,7 +1,6 @@
 package serenityswag.authentication;
 
 import net.serenitybdd.annotations.Managed;
-import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import org.junit.jupiter.api.Test;
@@ -13,24 +12,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SerenityJUnit5Extension.class)
 public class WhenLoggingOn extends UIInteractionSteps {
 
-    @Managed
-    WebDriver driver;
+//    @Managed
+//    WebDriver driver;
 
     @Test
     public void usersCanLogOnViaTheHomePage() {
 
-//        driver.get("https://www.saucedemo.com");
-          openUrl("https://www.saucedemo.com");
-//        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
-//        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-//        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
-        find("[data-test='username']").sendKeys("standard_user");
-        find("[data-test='password']").sendKeys("secret_sauce");
-        find("[data-test='login-button']").click();
+        openUrl("https://www.saucedemo.com");
+        $("[data-test='username']").sendKeys("standard_user");
+        $("[data-test='password']").sendKeys("secret_sauce");
+        $("[data-test='login-button']").click();
 
         // Should see product catalog
-
-        assertThat(find(".title").getText())
+        assertThat($(".title").getText())
                 .isEqualToIgnoringCase("Products");
     }
 }
