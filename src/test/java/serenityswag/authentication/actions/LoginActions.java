e14d3c1c5ab7b95a8634f2798d85fe592ce90465
EnigmaSaikat1 <saikat.j.das@gmail.com>
2025-08-15 11:12:13 +0530

    Impliment of page object model

--- src/test/java/serenityswag/authentication/actions/LoginActions.java
+++ src/test/java/serenityswag/authentication/actions/LoginActions.java
@@ -1,12 +1,54 @@
 package serenityswag.authentication.actions;
 
+import net.serenitybdd.annotations.Step;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 
 public class LoginActions extends UIInteractionSteps {
 
+    @Step("Log in as a standard user")
     public void asAStandardUser() {
         openUrl("https://www.saucedemo.com");
         $("[data-test='username']").sendKeys("standard_user");
+        $("[data-test='password']").sendKeys("secret_sauce");
+        $("[data-test='login-button']").click();
+    }
+
+    @Step("Log in as a locked out user")
+    public void asALockedOutUser() {
+        openUrl("https://www.saucedemo.com");
+        $("[data-test='username']").sendKeys("locked_out_user");
+        $("[data-test='password']").sendKeys("secret_sauce");
+        $("[data-test='login-button']").click();
+    }
+
+    @Step("Log in as a problem user user")
+    public void asAProblemUser() {
+        openUrl("https://www.saucedemo.com");
+        $("[data-test='username']").sendKeys("problem_user");
+        $("[data-test='password']").sendKeys("secret_sauce");
+        $("[data-test='login-button']").click();
+    }
+
+    @Step("Log in as a performance glitch user")
+    public void asAPerformanceGlitchUser() {
+        openUrl("https://www.saucedemo.com");
+        $("[data-test='username']").sendKeys("performance_glitch_user");
+        $("[data-test='password']").sendKeys("secret_sauce");
+        $("[data-test='login-button']").click();
+    }
+
+    @Step("Log in as a error user")
+    public void asAErrorUser() {
+        openUrl("https://www.saucedemo.com");
+        $("[data-test='username']").sendKeys("error_user");
+        $("[data-test='password']").sendKeys("secret_sauce");
+        $("[data-test='login-button']").click();
+    }
+
+    @Step("Log in as a visual user")
+    public void asAVisualUser() {
+        openUrl("https://www.saucedemo.com");
+        $("[data-test='username']").sendKeys("visual_user");
         $("[data-test='password']").sendKeys("secret_sauce");
         $("[data-test='login-button']").click();
     }
